cb60943bf9f2fdb6dd26d43e6f15259ce4b14446
Lazy Penguin <65771215+Robot-Penguin@users.noreply.github.com>
2020-06-28 14:06:04 +0800

    remove_comment
    removed commented part

--- sys_info.sh
+++ sys_info.sh
@@ -75,19 +75,6 @@ _EOF_
     return
 }    
 
-#    if [[ $(id -u) -eq 0 ]]; then
-#        cat <<- _EOF_
-#            <H2 style="background-color: 27a8fa">Home Utilization (All User)</H2>
-#            <PRE>$(du -sh /home/*)</PRE>
-#_EOF_
-#    else 
-#        cat <<- _EOF_
-#            <H2 style="background-color: 27a8fa">Home Space Utilization ($USER)</H2>
-#            <PRE>$(du -sh $HOME)</PRE>
-#_EOF_
-#    fi
-#    return
-
 usage () {
     echo "$PROGNAME: usage: $PROGNAME [ -f file | -i ]"
     return
